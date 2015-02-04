c16731749dde028caed85a801952214e66c9aafd
Andreas Göransson <andreas.goransson@mah.se>
2015-02-04 16:54:39 +0100

    Updated error messages in filters

--- src/Wetcat/Neo/filters.php
+++ src/Wetcat/Neo/filters.php
@@ -12,32 +12,73 @@
 */
 
 
+
+
+try {
+  $data = [
+    'error' => false,
+    'data'  => Neo::getUserProvider()->findAll(),
+    'code'  => 200
+  ];
+  $code = 200;
+  
+} catch ( Neoxygen\NeoClient\Exception\Neo4jException $e) {
+  $data = [
+    'error' => true,
+    'data'  => [$e->getMessage()],
+    'code'  => 404
+    ];
+  $code = 404;
+}
+
+return Response::json($data, $code);
+
+
+/*
+ * Filter that checks that the authentication token is valid, if the token is not 
+ * valid the filter aborts and resturns a JSON error.
+ *
+ * {
+ *   "error": true,
+ *   "data": {
+ *     ... error message/s ...
+ *   },
+ *   "code": 4xx
+ * }
+ */
 Route::filter('neoAuth', function ($route, $request){
 
   $token = $request->header('X-Auth-Token');
 
+  // If the token doesn't exist the user isn't authenticated
   if ( $token === null ) {
+    $data = ['Not authenticated'];
+    $code = 401;
+
     $response = Response::json([
       'error' => true,
-      'message' => 'Not authenticated',
-      'code' => 401
-      ], 401
+      'data' => $data,
+      'code' => $code
+      ], $code
     );
 
-    $response->header('Content-Type', 'application/json');
     return $response;
   }
 
+  // If the token is invalid the user isn't authenticated
   else if ( ! Neo::isAuthenticated($token) ) {
+    $data = ['Not authenticated'];
+    $code = 401;
+
     $response = Response::json([
       'error' => true,
-      'message' => 'Not authenticated',
-      'code' => 401
-      ], 401
+      'data' => $data,
+      'code' => $code
+      ], $code
     );
+
     return $response;
   }
-
 
 });
 
@@ -45,35 +86,48 @@ Route::filter('neoAdmin', function ($route, $request){
 
   $token = $request->header('X-Auth-Token');
 
+  // If the token doesn't exist the user isn't authenticated
   if ($token === null) {
+    $data = ['Not authenticated'];
+    $code = 401;
+
     $response = Response::json([
       'error' => true,
-      'message' => 'Not authenticated',
-      'code' => 401
-      ], 401
+      'data' => $data,
+      'code' => $code
+      ], $code
     );
 
-    $response->header('Content-Type', 'application/json');
     return $response;
   }
 
+  // If the token is invalid the user isn't authenticated
   else if ( ! Neo::isAuthenticated($token) ) {
+    $data = ['Not authenticated'];
+    $code = 401;
+
     $response = Response::json([
       'error' => true,
-      'message' => 'Not authenticated',
-      'code' => 401
-      ], 401
+      'data' => $data,
+      'code' => $code
+      ], $code
     );
+
     return $response;
   }
 
-  if( ! Neo::isAuthorized($token, 'admin') ){
+  // If the user is not a member of group Admin s/he doesn't have access
+  if( ! Neo::isAuthorized($token, 'Admin') ){
+    $data = ['Not authorized'];
+    $code = 401;
+
     $response = Response::json([
       'error' => true,
-      'message' => 'Not authorized',
-      'code' => 401
-      ], 401
+      'data' => $data,
+      'code' => $code
+      ], $code
     );
+
     return $response;
   }
 
